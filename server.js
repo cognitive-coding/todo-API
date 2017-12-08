153930c1c1c6253543603950fb58e039999fd6ca
Curts Hayes <cognitive-coding@protonmail.com>
2017-12-08 17:32:46 -0600

    add POST route

--- server.js
+++ server.js
@@ -1,20 +1,13 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const _ = require('underscore');
 const app = express();
 
 var PORT = process.env.PORT || 3000;
-var todos = [{
-    id: 1,
-    description: "Walk through the mists of Avalon",
-    completed: false
-}, {
-    id: 2,
-    description: "Write letter to ol Mr. Knox at Stateville",
-    completed: false
-}, {
-    id: 3,
-    description: "Get a little bit taller, and become a balla",
-    completed: true
-}];
+var todos = [];
+var todoNextId = 1;
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send("Todo API Root");
@@ -28,19 +21,30 @@ app.get('/todos', (req, res) => {
 // GET /todos/:id
 app.get('/todos/:id', (req, res) => {
     var todoId = parseInt(req.params.id, 10);  // all request params are strings
-    var matched;
-
-    todos.forEach((item) => {
-        if (todoId === item.id) {
-            matched = item;
-        }
-    });
+    var matched = _.findWhere(todos, {id: todoId});
 
     if (matched) {
         res.json(matched);
     } else {
         res.status(404).send();
     }
+});
+
+// POST
+app.post('/todos', (req, res) => {
+    var body = _.pick(req.body, 'description', 'completed');
+
+    if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+        return res.status(400).send();
+    }
+
+    body.description = body.description.trim();
+    
+    body.id = todoNextId++;
+    todos.push(body);
+    
+
+    res.json(todos);
 });
 
 app.listen(PORT, () => {
