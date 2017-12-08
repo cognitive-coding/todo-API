f60d3f0531173f1fc9c420fcebbaacc87f9a546a
Curts Hayes <cognitive-coding@protonmail.com>
2017-12-08 13:55:30 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send("Todo API Root");
+});
+
+app.listen(PORT, () => {
+    console.log('\n###############################################');
+    console.log('\n$$ Express Server is Running on port ' + PORT + ' $$\n');
+    console.log('###############################################\n');
+});
